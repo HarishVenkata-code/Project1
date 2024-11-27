f3fbe6be9d6515011e348724b2254c5a0e83ffa2
HarishVenkata-code <harishvenkata12@gmail.com>
2024-11-27 14:10:55 +0530

    code
    add code

---
+++ HF.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var greeting = "Hello, playground"
+var h = [1,2,3,4,5]
+var result = h.map { $0 * $0}
+print(result)
+let numbers = [1, 2, 3, 4, 5]
+let sum = numbers.reduce(0) { $0 + $1 }
+print(sum)
+let num1 = [[1, 2], [3, 4], [5, 6]]
+let sum1 = num1.flatMap({$0})
+print(sum1)
