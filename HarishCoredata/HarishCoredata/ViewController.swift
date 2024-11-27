1f30d489484fca3c3eb7b5c6a43a19896acf054f
HarishVenkata-code <harishvenkata12@gmail.com>
2024-11-27 14:25:16 +0530

    addes map

--- HarishCoredata/HarishCoredata/ViewController.swift
+++ HarishCoredata/HarishCoredata/ViewController.swift
@@ -11,7 +11,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        let numbers = [1, 2, 3, 4, 5]
+        let squares = numbers.map { $0 * $0 }
+
+        print(squares) 
     }
 
 
